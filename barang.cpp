9d1d345e7feac4e4a00de21131aa97190699813e
RifkyIkhsan <pratamarifky069-ship-it>
2026-04-21 16:17:20 +0700

    tambah barang.cpp tugas OOP

---
+++ barang.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+// Membuat class Barang
+class Barang{
+public:
+    string nama;
+    int jumlah;
+    string kategori;
+    string tanggalProduksi;
+
+    // Constructor
+    Barang(string n, int j, string k, string t){
+        nama = n;
+        jumlah = j;
+        kategori = k;
+        tanggalProduksi = t;
+    }
+
+    // Method untuk menampilkan data
+    void tampilkanData(){
+        cout << "Nama Barang       : " << nama << endl;
+        cout << "Jumlah            : " << jumlah << endl;
+        cout << "Kategori          : " << kategori << endl;
+        cout << "Tanggal Produksi  : " << tanggalProduksi << endl;
+        cout << "-----------------------------" << endl;
+    }
+};
+
+int main(){
+    // Membuat objek elektronik
+    Barang elektronik("Laptop", 10, "Elektronik", "10-03-2026");
+
+    // Membuat objek nonElektronik
+    Barang nonElektronik("Buku Tulis", 50, "Non-Elektronik", "01-02-2026");
+
+    // Menampilkan data
+    cout << "Data Barang Elektronik:\n";
+    elektronik.tampilkanData();
+
+    cout << "\nData Barang Non-Elektronik:\n";
+    nonElektronik.tampilkanData();
+
+    return 0;
+}
---
+++ p1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa{
+public:
+    int nim;
+    string nama;
+    float nilai;
+    void printdata(){
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
+};
+
+int main() {
+    Mahasiswa mhs;
+
+    mhs.nim = 111;
+    mhs.nama = "Joko Susilo";
+    mhs.nilai = 75.5;
+    mhs.printdata();
+}
